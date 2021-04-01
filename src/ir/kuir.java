3877bcdf30e01ae59db464e6a964874f0a479e4c
minhyeokseo <smhsmh1204@gmail.com>
2021-04-01 10:03:56 +0900

    5주차 실습 완료, indexer.java의 의도와 다르게 실행 되었던 부분 수정

--- src/ir/kuir.java
+++ src/ir/kuir.java
@@ -14,17 +14,19 @@ public class kuir {
 			makeCollection.makeXml("C:\\Users\\82103\\SimpleIR\\src\\data");
 			makeKeyword.makeKkma("C:\\Users\\82103\\SimpleIR\\src\\collection.xml");
 			indexer.makePost("C:\\Users\\82103\\SimpleIR\\src\\index.xml");
+			search.CalcSim("C:\\Users\\82103\\SimpleIR\\src\\index.post", "떡 감자 찹쌀 아이스크림 파스타");
 		} else {
 			if (args[0].equals("-c")) {
 				String dirPath = "C:\\Users\\82103\\SimpleIR\\src\\" + args[1];
 				makeCollection.makeXml(dirPath);
-			} 
-			else if (args[0].equals("-k")) {
+			} else if (args[0].equals("-k")) {
 				makeKeyword.makeKkma(args[1]);
-			} 
-			else if (args[0].equals("-i")) {
+			} else if (args[0].equals("-i")) {
 				indexer.makePost(args[1]);
+			} else if (args[0].equals("-s") && args[2].equals("-q")) {
+				search.CalcSim(args[1], args[3]);
 			}
 		}
 	}
+
 }
---
+++ src/ir/search.java
@@ -0,0 +1,109 @@
+package ir;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.HashMap;
+import java.util.Iterator;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.snu.ids.kkma.index.Keyword;
+import org.snu.ids.kkma.index.KeywordExtractor;
+import org.snu.ids.kkma.index.KeywordList;
+
+public class search {
+	@SuppressWarnings("unchecked")
+	public static void CalcSim(String postPath, String query) throws IOException, ClassNotFoundException {
+		FileInputStream fin = new FileInputStream(postPath);
+		ObjectInputStream ois = new ObjectInputStream(fin);
+		Object object = ois.readObject();
+		ois.close();
+
+		HashMap<String, String> hashmap = (HashMap<String, String>) object;
+		Iterator<String> it = hashmap.keySet().iterator();
+
+		String input = query;
+		
+		KeywordExtractor ke = new KeywordExtractor();
+		KeywordList kl = ke.extractKeyword(input, true);
+		int[] TF = new int[kl.size()];
+		double[] similar = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+
+		for (int i = 0; i < kl.size(); i++) {
+			Keyword kwrd = kl.get(i);
+			System.out.println(i+1+"번 형태소 : "+kwrd.getString());
+			//TF[i] = kwrd.getCnt();
+			TF[i] = 1;
+		}
+		
+		for (int i = 0; i < similar.length-1; i++) {
+			System.out.println("========"+i+"번 문서========");
+			for (int j = 0; j < kl.size(); j++) {
+				String value = hashmap.get(kl.get(j).getString());
+				if (value != null) {
+					String[] weight = value.split(" ");
+					similar[i] += TF[j] * Double.valueOf((weight[2 * i + 1]));
+					System.out.println(j+1+"번 형태소의 가중치 : "+weight[2*i+1]);
+				}
+			}
+		}
+		int[] zero = new int[5];
+		int cnt=0;
+		System.out.println("========유사도========");
+		for(int i=0; i<5; i++) {
+			System.out.println(i+"번 문서와의 유사도 : "+similar[i]);
+			if(similar[i] == 0.0)
+				zero[cnt++] = i;
+		}
+		System.out.println("========유사도 상위 문서========");
+		
+		int first = 5;
+		int second = 5;
+		int third = 5;
+
+		for (int i = 0; i < similar.length-1; i++)
+			if (similar[first] < similar[i])
+				first = i;
+
+		for (int i = 0; i < similar.length-1; i++)
+			if (similar[second] < similar[i])
+				if (i != first)
+					second = i;
+
+		for (int i = 0; i < similar.length-1; i++)
+			if (similar[third] < similar[i])
+				if (i != first && i != second)
+					third = i;
+		
+		if(first == 5 && second == 5 && third == 5) {
+			first = zero[0];
+			second =zero[1];
+			third =zero[2];
+		}
+		else if(second == 5 && third == 5) {
+			second = zero[0];
+			third = zero[1];
+		}
+		else if(third == 5)
+			third = zero[0];
+		
+		File xml = new File("C:\\Users\\82103\\SimpleIR\\src\\collection.xml");
+		FileReader fr = new FileReader(xml);
+
+		String str = "";
+		int ch = fr.read();
+		while (ch != -1) {
+			str += (char) ch;
+			ch = fr.read();
+		}
+
+		Document hp = Jsoup.parse(str);
+		
+		System.out.println("유사도 1위 문서의 제목 : "+hp.getElementById(String.valueOf(first)).getElementsByTag("title").text());
+		System.out.println("유사도 2위 문서의 제목 : "+hp.getElementById(String.valueOf(second)).getElementsByTag("title").text());
+		System.out.println("유사도 3위 문서의 제목 : "+hp.getElementById(String.valueOf(third)).getElementsByTag("title").text());
+	}
+
+}
